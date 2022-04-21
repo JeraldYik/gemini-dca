9ee505cabd386fd8f59820745fac1c7b72f25e64
jeraldyik <jeraldyik12@gmail.com>
2022-04-21 10:47:09 +0800

    chore: add unit test for db bulk insert

---
+++ test/unit/database/bulkInsert.test.ts
@@ -0,0 +1,72 @@
+import Order, {
+  OrderAttributes,
+} from "../../../src/services/database/models/order";
+
+import { Sequelize } from "sequelize";
+import bulkInsertRowIntoDb from "../../../src/services/database/bulkInsertRowIntoDb";
+import { expect } from "chai";
+import { sequelize } from "./../../../src/utils/setup";
+import { startOfDay } from "date-fns";
+
+describe("UNIT TEST: Bulk Insert into database", async () => {
+  let sequelizeInstance: Sequelize;
+  before(() => {
+    sequelizeInstance = sequelize;
+  });
+  it("Should successfully bulk insert", async () => {
+    const VALUES: OrderAttributes[] = [
+      {
+        ticker: "btcsgd",
+        createdForDay: startOfDay(new Date()),
+        fiatDepositInSgd: 10,
+        pricePerCoinInSgd: 100,
+        coinAmount: 1,
+      },
+      {
+        ticker: "ethsgd",
+        createdForDay: startOfDay(new Date()),
+        fiatDepositInSgd: 20,
+        pricePerCoinInSgd: 100,
+        coinAmount: 1,
+      },
+    ];
+
+    await bulkInsertRowIntoDb(VALUES);
+    const btcSgdRow = await Order.findOne({
+      where: {
+        ticker: "btcsgd",
+      },
+    });
+    const ethSgdRow = await Order.findOne({
+      where: {
+        ticker: "ethsgd",
+      },
+    });
+    expect(btcSgdRow).to.exist;
+    expect(btcSgdRow!.get().ticker).to.be.eql("btcsgd");
+    expect(btcSgdRow!.get().createdForDay).to.be.eql(startOfDay(new Date()));
+    expect(btcSgdRow!.get().fiatDepositInSgd).to.be.eql(10);
+    expect(btcSgdRow!.get().pricePerCoinInSgd).to.be.eql(100);
+    expect(btcSgdRow!.get().coinAmount).to.be.eql(1);
+
+    expect(ethSgdRow).to.exist;
+    expect(ethSgdRow!.get().ticker).to.be.eql("ethsgd");
+    expect(ethSgdRow!.get().createdForDay).to.be.eql(startOfDay(new Date()));
+    expect(ethSgdRow!.get().fiatDepositInSgd).to.be.eql(20);
+    expect(ethSgdRow!.get().pricePerCoinInSgd).to.be.eql(100);
+    expect(ethSgdRow!.get().coinAmount).to.be.eql(1);
+  });
+
+  after(async () => {
+    await Order.destroy({
+      where: {
+        ticker: "btcsgd",
+      },
+    });
+    await Order.destroy({
+      where: {
+        ticker: "ethsgd",
+      },
+    });
+  });
+});
