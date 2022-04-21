dbae7621f31cffadbb33f5eea8d0f9b6b14cedca
jeraldyik <jeraldyik12@gmail.com>
2022-04-21 10:31:47 +0800

    chore: change to bulk insert into db

--- src/index.ts
+++ src/index.ts
@@ -1,73 +1,100 @@
+import { differenceInCalendarDays, startOfDay } from "date-fns";
+import { googleSheetName, startDate } from "./utils/config";
+
 import { TICKERS } from "./utils/constants";
 import bluebird from "bluebird";
+import bulkInsertRowIntoDb from "./services/database/bulkInsertRowIntoDb";
 import checkIfOrderIsFulfilled from "./services/gemini/checkIfOrderIsFulfilled";
 import createNewOrder from "./services/gemini/createNewOrder";
-import { differenceInCalendarDays } from "date-fns";
 import { flatten } from "lodash";
 import getTickerBestBidPrice from "./services/gemini/getTickerBestBidPrice";
 import { initialiseGoogleDocument } from "./utils/setup";
-import insertRowIntoDb from "./services/database/insertRowIntoDb";
 import { logger } from "./utils/logger";
-import { startDate } from "./utils/config";
 import updateCells from "./services/googleSheets/updateCells";
 
 const main = async () => {
-  // const todayDate = new Date().toLocaleDateString();
-  // const unflattenedValues = await bluebird.map(
-  //   Object.entries(TICKERS),
-  //   async (coinInfo) => {
-  //     const tickerMetadata = coinInfo[1];
-  //     const tickerBestBidPrice = await getTickerBestBidPrice(
-  //       tickerMetadata.symbol
-  //     );
-  //     const newOrder = await createNewOrder(tickerMetadata, tickerBestBidPrice);
+  const todayDate = new Date().toLocaleDateString();
+  const unflattenedTransactionValues = await bluebird.map(
+    Object.entries(TICKERS),
+    async (coinInfo) => {
+      const tickerMetadata = coinInfo[1];
+      const tickerBestBidPrice = await getTickerBestBidPrice(
+        tickerMetadata.symbol
+      );
+      const newOrder = await createNewOrder(tickerMetadata, tickerBestBidPrice);
 
-  //     if (newOrder.is_cancelled) {
-  //       logger.error({
-  //         message: "Order has been cancelled upon creation",
-  //         meta: { ...newOrder },
-  //       });
-  //     }
+      if (newOrder.is_cancelled) {
+        logger.error({
+          message: "Order has been cancelled upon creation",
+          meta: { ...newOrder },
+        });
+      }
 
-  //     // order fulfilled
-  //     if (!newOrder.is_live)
-  //       return [
-  //         todayDate,
-  //         tickerMetadata.dailyFiatAmount,
-  //         newOrder.avg_execution_price,
-  //         newOrder.executed_amount,
-  //       ];
+      // order fulfilled
+      if (!newOrder.is_live)
+        return [
+          todayDate,
+          tickerMetadata.dailyFiatAmount,
+          newOrder.avg_execution_price,
+          newOrder.executed_amount,
+        ];
 
-  //     const fulfilledOrder = await checkIfOrderIsFulfilled(newOrder.order_id);
+      const fulfilledOrder = await checkIfOrderIsFulfilled(newOrder.order_id);
 
-  //     logger.info({
-  //       message: "Order fulfilled",
-  //       meta: {
-  //         ...fulfilledOrder,
-  //       },
-  //     });
-  //     return [
-  //       todayDate,
-  //       tickerMetadata.dailyFiatAmount,
-  //       fulfilledOrder!.avg_execution_price,
-  //       fulfilledOrder!.executed_amount,
-  //     ];
-  //   },
-  //   { concurrency: 1 }
-  // );
-  // const flattenedValues = flatten(unflattenedValues);
+      logger.info({
+        message: "Order fulfilled",
+        meta: {
+          ...fulfilledOrder,
+        },
+      });
+      return [
+        todayDate,
+        tickerMetadata.dailyFiatAmount,
+        fulfilledOrder!.avg_execution_price,
+        fulfilledOrder!.executed_amount,
+      ];
+    },
+    { concurrency: 1 }
+  );
+  const flattenedTransactionValues = flatten(unflattenedTransactionValues);
 
-  // const doc = await initialiseGoogleDocument();
+  const doc = await initialiseGoogleDocument();
 
-  // const splitStartDate = startDate.split("/").map((d) => parseInt(d));
-  // const differenceInDays = differenceInCalendarDays(
-  //   new Date(),
-  //   new Date(splitStartDate[2], splitStartDate[1] - 1, splitStartDate[0])
-  // );
+  const splitStartDate = startDate.split("/").map((d) => parseInt(d));
+  const differenceInDays = differenceInCalendarDays(
+    new Date(),
+    new Date(splitStartDate[2], splitStartDate[1] - 1, splitStartDate[0])
+  );
 
-  // await updateCells(doc, "Test", differenceInDays, flattenedValues);
+  if (differenceInDays < 0) {
+    logger.error({
+      message: "Start date of recording is later than today",
+      meta: {
+        startDate,
+      },
+    });
+  }
 
-  await insertRowIntoDb();
+  await updateCells(
+    doc,
+    googleSheetName,
+    differenceInDays,
+    flattenedTransactionValues
+  );
+
+  const bulkCreateTransactionRow = unflattenedTransactionValues.map(
+    (transactionRow, idx) => {
+      return {
+        ticker: Object.values(TICKERS)[idx].symbol,
+        createdForDay: startOfDay(new Date()),
+        fiatDepositInSgd: transactionRow[1] as number,
+        pricePerCoinInSgd: transactionRow[2] as number,
+        coinAmount: transactionRow[3] as number,
+      };
+    }
+  );
+
+  await bulkInsertRowIntoDb(bulkCreateTransactionRow);
 };
 
 main();
