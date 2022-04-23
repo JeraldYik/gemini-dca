629d2939f1dc16380229ae532fbc6da7c64cf4c6
jeraldyik <jeraldyik12@gmail.com>
2022-04-23 17:40:23 +0800

    chore: change order price to 2% below best bid price

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,6 +1,6 @@
 import { Ticker } from "../../types";
 
-export const ORDER_PRICE_TO_BID_PRICE_RATIO = 0.999;
+export const ORDER_PRICE_TO_BID_PRICE_RATIO = 0.998;
 export const TICKERS: Ticker = {
   BTC: {
     symbol: "btcsgd",
