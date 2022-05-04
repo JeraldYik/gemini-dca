c13a4b2d7020f8fb9cf3f53c146bac53b4b5c323
jeraldyik <jeraldyik12@gmail.com>
2022-05-04 20:53:22 +0800

    fix: reduce daily dca fiat amount by 50%

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -5,12 +5,12 @@ export const ORDER_PRICE_TO_BID_PRICE_RATIO = 0.999;
 export const TICKERS: Ticker = {
   BTC: {
     symbol: "btcsgd",
-    dailyFiatAmount: 10,
+    dailyFiatAmount: 5,
     googleSheetCellRange: "E*:H*",
   },
   ETH: {
     symbol: "ethsgd",
-    dailyFiatAmount: 20,
+    dailyFiatAmount: 10,
     googleSheetCellRange: "I*:L*",
   },
 } as const;
