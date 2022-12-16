cf8b0f7deed87256a81bd4587b3d1b7b2f260843
jeraldyik <jeraldyik12@gmail.com>
2022-12-16 19:32:12 +0800

    fix: get order status retry

--- src/services/gemini/getOrderStatus.ts
+++ src/services/gemini/getOrderStatus.ts
@@ -1,26 +1,43 @@
 import { OrderStatus } from "../../../types";
+import delay from "../../utils/delay";
 import { logger } from "./../../utils/logger";
 import { restClient } from "../../setup/gemini";
 
 const getOrderStatus = async (orderId: string): Promise<OrderStatus> => {
   let orderStatusData: OrderStatus;
-  try {
-    orderStatusData = await restClient.getMyOrderStatus({ order_id: orderId });
+  const MAX_COUNTER = 3;
+  let counter = 1;
+  while (counter <= MAX_COUNTER) {
     logger.info({
-      message: "Order Status data",
-      meta: {
-        orderId,
-        ...orderStatusData,
-      },
-    });
-  } catch (error) {
-    logger.error({
-      message: "Failed to get order status data",
+      message: `Getting order status data. attempt=${counter}`,
       meta: {
         orderId,
       },
-      error,
     });
+
+    try {
+      orderStatusData = await restClient.getMyOrderStatus({
+        order_id: orderId,
+      });
+      logger.info({
+        message: "Order Status data",
+        meta: {
+          orderId,
+          ...orderStatusData,
+        },
+      });
+      break;
+    } catch (error) {
+      logger.error({
+        message: "Failed to get order status data",
+        meta: {
+          orderId,
+        },
+        error,
+      });
+    }
+    counter++;
+    await delay(1000);
   }
 
   return orderStatusData!;
